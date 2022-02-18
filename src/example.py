9147bd9358e603b74732fd2fb637f8f0f04f9a96
Mike <okibeogezi@outlook.com>
2022-02-18 13:00:21 -0700

    chore: added assets

--- src/example.py
+++ src/example.py
@@ -6,9 +6,11 @@ import pathlib
 from gazebo import Gazebo
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image, LaserScan
+from nav_msgs.msg import Odometry
 import cv2
 from cv_bridge import CvBridge
 import time
+import tf
 
 IMG_WIDTH = 400
 IMG_HEIGHT = 400
@@ -25,19 +27,26 @@ class Main():
         self.gazebo = Gazebo()
         self.bridge = CvBridge()
 
+        self.base_frame = rospy.get_param('~base_frame', '/base_link')
+        self.odom_frame = rospy.get_param('~odom_frame', '/odom')
+        self.cam_frame = '/camera/image_raw'
+        self.scan_frame = '/scan'
+
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
-        self.laser_scan = rospy.Subscriber('/scan', LaserScan, self.scan_cb)
-        self.cam = rospy.Subscriber('/camera/image_raw', Image, self.image_cb)
+        self.laser_scan = rospy.Subscriber(self.scan_frame, LaserScan, self.scan_cb)
+        self.odom = rospy.Subscriber(self.odom_frame, Odometry, self.odom_cb)
+        self.cam = rospy.Subscriber(self.cam_frame, Image, self.image_cb)
 
         self.logged_res = False
         self.latest_image = None
+        self.latest_odom_reading = None
         self.latest_scan_reading = None
 
         mv = Twist()
-        while (time.time() - self.start_time) < 5:
-            mv.linear.x = 0.1
-            mv.angular.z = 0.0
+        while (time.time() - self.start_time) < 30:
+            mv.linear.x = 0.0
+            mv.angular.z = 0.5
             self.cmd_vel.publish(mv)
             self.r.sleep()
         self.stop()
@@ -46,7 +55,12 @@ class Main():
         rospy.loginfo('Resetting...')
 
     def scan_cb(self, msg):
+        rospy.loginfo('Receiving scanning message...')
         self.latest_scan_reading = None
+
+    def odom_cb(self, msg):
+        rospy.loginfo('Receiving odometry message...')
+        self.latest_odom_reading = None
 
     def image_cb(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
